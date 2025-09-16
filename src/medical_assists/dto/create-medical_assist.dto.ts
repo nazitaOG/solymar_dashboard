14f80fbeae41d51b0590cf2f120c212de7dbed48
nazitaOG <lopeznazarenoo@gmail.com>
2025-09-15 23:07:09 -0300

    dto medical assist

--- src/medical_assists/dto/create-medical_assist.dto.ts
+++ src/medical_assists/dto/create-medical_assist.dto.ts
@@ -5,27 +5,41 @@ import {
   IsString,
   IsUUID,
   IsOptional,
+  IsNotEmpty,
+  MaxLength,
 } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { toTrim } from '../../common/transformers';
 
 export class CreateMedicalAssistDto {
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   @Max(Number.MAX_SAFE_INTEGER)
   totalPrice: number;
 
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   @Max(Number.MAX_SAFE_INTEGER)
   amountPaid: number;
 
+  @Transform(toTrim, { toClassOnly: true })
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
   bookingReference: string;
 
+  @Transform(toTrim, { toClassOnly: true })
   @IsString()
   @IsOptional()
-  assistType: string;
+  @MaxLength(128)
+  assistType?: string;
 
+  @Transform(toTrim, { toClassOnly: true })
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(128)
   provider: string;
 
   @IsUUID()
--- src/medical_assists/dto/update-medical_assist.dto.ts
+++ src/medical_assists/dto/update-medical_assist.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMedicalAssistDto } from './create-medical_assist.dto';
+import { AtLeastOneField } from '../../common/validators/at-lest-one-field';
 
-export class UpdateMedicalAssistDto extends PartialType(CreateMedicalAssistDto) {}
+export class UpdateMedicalAssistDto extends PartialType(
+  CreateMedicalAssistDto,
+) {
+  @AtLeastOneField([
+    'totalPrice',
+    'amountPaid',
+    'bookingReference',
+    'assistType',
+    'provider',
+    'reservationId',
+  ])
+  private _atLeastOne!: true;
+}
